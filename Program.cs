78cf8580c45ceb294ee02fb4e3a0a343c70708d8
Joby John <joby432john@gmail.com>
2022-08-10 12:26:05 +0530

    changed plan from website to adding chapters as text and seraching in it

--- Program.cs
+++ Program.cs
@@ -1,7 +1,7 @@
 ﻿// Trying to create a study material finder that helps in finding study material according to requirement
 // no idea how im gonna do it anyways just starting , will find way on the way
 
-//functions to implement 1. serachcond, main, weblist as of now 
+//functions to implement 1. serachcond, main, textbooks as of now 
 
 SearchCond(); //calls the searchcond function 
 static void SearchCond() // includes search conditions selects subjects
@@ -83,14 +83,58 @@ static void Social()
 
 static void printresult( string subject, string Term)
     {
-    // function to search on the websites in website list based on parameters subject and term and return the results
-    //string Result = Websites( subject , term);
-    //Console.WriteLine(Result);//prints the result
-    Console.WriteLine("For term :" + Term + ", in subject : " + subject + " nothing found");
+    // function to search on the textbooks in textbook list based on parameters subject and term and return the results
+    string Result = Textbook( subject , Term);
+    Console.WriteLine(Result);//prints the result
+    if (Result == null)
+    {
+        Console.WriteLine("\n\nFor term :" + Term + ", in subject : " + subject + " search unsuccessfull");
+    }
+    else
+    {
+        Console.WriteLine("\n\nFor term :" + Term + ", in subject : " + subject + " search successfull");
+    }
 }
 
-//static string Websites(string subject, string term)
-//{
-//searches somehow in websites and returns
-// Return result;
-//}
+static string Textbook (string subject, string term)
+{
+    string path = @"C:\Users\Tony Stark\source\repos\SMS-study_material_searcher\chapterfiles\1.txt";
+    //searches somehow in textbooks and returns 
+    string result = ("Not Found ");
+    switch (subject)
+    {
+        case "physics" :
+            Console.WriteLine("physics");
+            break;
+        case "chemistry":
+            Console.WriteLine("chemistry");
+            break;
+        case "biology":
+            Console.WriteLine("biology");
+            break;
+        case "maths":
+            Console.WriteLine("chemistry");
+            break;
+        case "social":
+             string text = File.ReadAllText(path);
+            text = text.ToLower();
+            int loc = text.IndexOf(term);//finds index of first occurance of the term
+            if (loc == -1)
+            {
+                result = ("Term not Found");
+            }
+            else
+            {
+                int n = 1;
+                Console.WriteLine(n + " Term Found " );
+                result = text.Substring(loc, loc + 600);//passes a string of 600 indexes to result  
+            }
+            Console.WriteLine("social");
+            break;
+        default:
+            Console.WriteLine(result);
+            break;
+    }
+    string Result = (result);
+ return Result;
+}
